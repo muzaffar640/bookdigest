f64f3f4266fded696f94c774bd89ff7053583ec5
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-08 17:15:45 +0530

    Add registerUser and loginUser routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,48 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(req.body);
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+module.exports = { authUser, registerUser };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+const userRoutes = require("./v1/routes/userRoutes");
+const connectDB = require("./config/db");
+
 const port = process.env.PORT || 8000;
 
-dotenv.config();
+connectDB();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/users", userRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
