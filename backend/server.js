d3f743796a58476a4d0aea149ecc225e816b518c
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-08 17:33:28 +0530

    Custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const userRoutes = require("./v1/routes/userRoutes");
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 8000;
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/users", userRoutes);
-
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
