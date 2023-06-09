3b1c38da88cbda51480706323db3a9050c3a8e52
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-09 16:50:25 +0530

    Add custom auth middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 const userRoutes = require("./v1/routes/userRoutes");
 const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 8000;
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
 app.use("/api/v1/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
