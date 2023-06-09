5fc689f3a664df66c610a0433e77fef5e9d5e1a6
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-09 16:54:49 +0530

    Add logout and update end point

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,6 +13,8 @@ const authUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      role: user.role,
+      profilePic: user.profilePic,
     });
   } else {
     res.status(401);
@@ -30,7 +32,16 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exist");
   }
 
-  const user = await User.create({ name, email, password });
+  const user = await User.create({
+    name,
+    email,
+    password,
+    //Note: don't wanna hard delete a user
+    isDeleted: false,
+    role: "user",
+    //Note: Uploading profile picture is an upcoming feature
+    profilePic: "",
+  });
 
   if (user) {
     generateToken(res, user._id);
@@ -38,6 +49,9 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      isDeleted: user.isDeleted,
+      role: user.role,
+      profilePic: user.profilePic,
     });
   } else {
     res.status(400);
@@ -45,4 +59,41 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { authUser, registerUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User Logged Out" });
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  console.log("update", req, req.user);
+
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).send({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  //TODO: update the user isDelete parameter to true
+});
+
+module.exports = { authUser, registerUser, logoutUser, updateUser, deleteUser };
--- backend/v1/routes/userRoutes.js
+++ backend/v1/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { authUser, registerUser } = require("../../controllers/userController");
+const {
+  authUser,
+  registerUser,
+  logoutUser,
+  updateUser,
+} = require("../../controllers/userController");
+const protect = require("../../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", protect, logoutUser);
+router.put("/profile", protect, updateUser);
 
 module.exports = router;
