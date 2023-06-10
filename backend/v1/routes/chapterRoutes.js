7f07697f6e29d35ac5c804e089e671fd539d889b
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-11 01:37:46 +0530

    Update chapter routes

--- backend/v1/routes/chapterRoutes.js
+++ backend/v1/routes/chapterRoutes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const {
   createNewChapter,
-  getAllChaptersForOneBook,
-  getOneChapter,
   updateOneChapter,
   deleteOneChapter,
 } = require("../../controllers/chapterController");
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/", getAllChaptersForOneBook);
-router.get("/:chapterId", getOneChapter);
 router.post("/", createNewChapter);
 router.put("/:chapterId", updateOneChapter);
 router.delete("/:chapterId", deleteOneChapter);
