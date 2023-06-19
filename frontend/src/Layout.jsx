e1e42e0e0d3f47a69cb4284f1e0a068a9da43906
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-19 19:31:46 +0530

    Add flowbite configuration

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./components/Header";
 import { SideBar } from "./components/SideBar";
 import { Outlet } from "react-router-dom";
-
+import { Flowbite } from "flowbite-react";
 export const Layout = () => {
   return (
-    <>
-      <Header />
-      <SideBar />
-      <Outlet />
-    </>
+    <div className="h-screen p-5 bg-white dark:bg-gray-900 antialiased">
+      <Flowbite>
+        <Header />
+        <SideBar />
+        <Outlet />
+      </Flowbite>
+    </div>
   );
 };
